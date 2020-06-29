1b9b468857b0a17226ca73a6c3736358ccb8253e
Eduardo-Baranowski <eduardobaranowski@gmail.com>
2020-06-29 14:14:13 -0300

    front

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Title } from './styles';
+import api from '../../services/api';
+
+const Dashboard: React.FC = () => {
+  return <Title>Dashboard</Title>;
+};
+
+export default Dashboard;
